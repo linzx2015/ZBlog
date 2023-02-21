3bbbca52a1a011883d2b25bce6ba8a57378a4cbf
lucas.lin <857720446@qq.com>
2023-02-21 21:41:53 +0800

    增加注释

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 /**
+ * 先启动后台，再npm install,npm run dev打包启动前端vue项目
  * 前端vue项目配置8080端口：http://127.0.0.1:8080/#/home
  * 通过index.js的proxyTable代理访问后台服务http://127.0.0.1:8086/#/home
  * */
